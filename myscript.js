1c3a3e38b155b96ab0f442de5508af07ce48b97b
0715431007-hue <0715431007@s.tusd1.org>
2026-04-27 15:48:06 +0000

    commit and push

--- myscript.js
+++ myscript.js
@@ -6,8 +6,8 @@ let popupContent = document.getElementById("popupText");
 
 // Get hint buttons
 let vowelButton = document.getElementById("vowelHint");
-let letterRButton = document.getElementById("letterRHint");
-let fiveLetterButton = document.getElementById("fiveLetterHint");
+let letterButton = document.getElementById("letterHint");
+let ConsonantButton = document.getElementById("consonantHint");
 
 // Show popup function
 function showPopup() {
@@ -20,10 +20,72 @@ function hidePopup() {
     popup.style.display = "none";
 }
 
-// Connect close button
 closeButton.addEventListener("click", hidePopup);
 
 // Connect the buttons to functions
 vowelButton.addEventListener("click", showPopup);
-letterRButton.addEventListener("click", showPopup);
-fiveLetterButton.addEventListener("click", showPopup);
+letterButton.addEventListener("click", showPopup);
+ConsonantButton.addEventListener("click", showPopup);
+
+// Functions to show different minigames
+function showVowelGame() {
+    minigameArea.innerText = "Vowel minigame goes here!";
+    popup.style.display = "block";
+}
+
+function showLetterRGame() {
+    minigameArea.innerText = "Letter minigame goes here!";
+    popup.style.display = "block";
+}
+
+function showFiveLetterGame() {
+    minigameArea.innerText = "Consonant minigame goes here!";
+    popup.style.display = "block";
+}
+
+function hidePopup() {
+    popup.style.display = "none";
+}
+
+// Connect each button to its own minigame
+vowelButton.addEventListener("click", showVowelGame);
+letterButton.addEventListener("click", showLetterRGame);
+ConsonantButton.addEventListener("click", showFiveLetterGame);
+
+// Connect close button
+closeButton.addEventListener("click", hidePopup);
+
+// Array of possible words
+let wordList = ["APPLE", "BEACH", "CRANE", "DREAM", "EAGLE", "FLAME", "GRAPE", "HORSE", "IMAGE", "JUICE"];
+
+// Function to pick random word
+function getRandomWord() {
+    let randomIndex = Math.floor(Math.random() * wordList.length);
+    return wordList[randomIndex];
+}
+
+// Set the secret word when page loads
+let secretWord = getRandomWord();
+
+console.log("Secret word is:", secretWord); // Remove this later - just for testing!
+
+// Get input elements
+let guessInput = document.getElementById("guessInput");
+let submitButton = document.getElementById("submitGuess");
+let feedback = document.getElementById("feedback");
+
+function checkGuess() {
+    let playerGuess = guessInput.value.toUpperCase();
+    
+    if (playerGuess === secretWord) {
+        feedback.innerText = "Correct! You won! The word was " + secretWord;
+    } else {
+        feedback.innerText = "Try again! Not " + playerGuess;
+    }
+    
+    // Clear the input for next guess
+    guessInput.value = "";
+}
+
+submitButton.addEventListener("click", checkGuess);
+
