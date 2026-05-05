0f0318340ceaa88ea4450aec1a63f881a1dc8196
0715431007-hue <0715431007@s.tusd1.org>
2026-05-05 15:46:43 +0000

    commit

--- myscript.js
+++ myscript.js
@@ -1,35 +1,20 @@
 console.log("Script started");
-// Get elements
+// popup for popup, close button for close button, popup content for content inside of the popup
 let popup = document.getElementById("popup");
 let closeButton = document.getElementById("closeBtn");
 let popupContent = document.getElementById("popupText");
 
-// Get hint buttons
+// hint buttons
 let vowelButton = document.getElementById("vowelHint");
 let letterButton = document.getElementById("letterHint");
 let ConsonantButton = document.getElementById("consonantHint");
 
+// current row and current position equal 0
 let currentRow = 0;
 let currentPosition = 0; 
 
+// player guess currently empty
 let playerGuess = "";
-
-
-// Functions to show different minigames
-function showVowelGame() {
-    minigameArea.innerText = "Vowel minigame goes here!";
-    popup.style.display = "block";
-}
-
-function showLetterGame() {
-    minigameArea.innerText = "Letter minigame goes here!";
-    popup.style.display = "block";
-}
-
-function showConsonantGame() {
-    minigameArea.innerText = "Consonant minigame goes here!";
-    popup.style.display = "block";
-}
 
 function hidePopup() {
     popup.style.display = "none";
@@ -44,7 +29,7 @@ ConsonantButton.addEventListener("click", showConsonantGame);
 closeButton.addEventListener("click", hidePopup);
 
 // Array of possible words
-let wordList = ["APPLE", "BEACH", "CRANE", "DREAM", "EAGLE", "FLAME", "GRAPE", "HORSE", "IMAGE", "JUICE", "BORED", "BOARD"];
+let wordList = ["APPLE", "BEACH", "CRANE", "DREAM", "EAGLE", "FLAME", "GRAPE", "HORSE", "IMAGE", "JUICE", "BORED", "BOARD", "HATER", "LATER", "FIGHT"];
 
 
 // Game variables
@@ -94,6 +79,10 @@ function submitGuess() {
         checkGuess();  // Check the guess and add colors
         currentRow = currentRow + 1;
         currentPosition = 0;
+        if (currentRow >= 6) {
+            // They used all 6 guesses - show the answer
+            alert("Game Over! The word was: " + secretWord);
+        }
         console.log("Moving to row:", currentRow);
     } else {
         console.log("Need 5 letters before submitting!");
@@ -213,4 +202,180 @@ function checkGuess() {
     if (playerGuess === secretWord) {
         alert("You won! The word was " + secretWord);
     }
+}
+
+function showVowelGame() {
+    // Pick a random math operation
+    let operations = ['+', '-', '*', '/'];
+    let randomOperation = operations[Math.floor(Math.random() * operations.length)];
+    
+    let num1, num2, correctAnswer;
+    
+    if (randomOperation === '+') {
+        num1 = Math.floor(Math.random() * 20) + 1;
+        num2 = Math.floor(Math.random() * 20) + 1;
+        correctAnswer = num1 + num2;
+    } else if (randomOperation === '-') {
+        num1 = Math.floor(Math.random() * 20) + 10; // Make sure num1 is bigger
+        num2 = Math.floor(Math.random() * 10) + 1;
+        correctAnswer = num1 - num2;
+    } else if (randomOperation === '*') {
+        num1 = Math.floor(Math.random() * 10) + 1;
+        num2 = Math.floor(Math.random() * 10) + 1;
+        correctAnswer = num1 * num2;
+    } else if (randomOperation === '/') {
+        // Make sure division gives a whole number
+        correctAnswer = Math.floor(Math.random() * 10) + 1;
+        num2 = Math.floor(Math.random() * 5) + 1;
+        num1 = correctAnswer * num2;
+    }
+    
+    minigameArea.innerHTML = `
+        <h3>Vowel Hint Minigame</h3>
+        <p>Solve this math problem to get a vowel hint:</p>
+        <p>${num1} ${randomOperation} ${num2} = ?</p>
+        <input type="number" id="vowelAnswer" placeholder="Your answer">
+        <button id="vowelSubmit">Submit</button>
+        <p id="vowelResult"></p>
+    `;
+    
+    popup.style.display = "block";
+    
+    // Connect the submit button
+    let vowelSubmitBtn = document.getElementById("vowelSubmit");
+    vowelSubmitBtn.addEventListener("click", function() {
+        let playerAnswer = document.getElementById("vowelAnswer").value;
+        let resultArea = document.getElementById("vowelResult");
+        
+        if (parseInt(playerAnswer) === correctAnswer) {
+            // Find vowels in secret word
+            let vowels = [];
+            for (let i = 0; i < secretWord.length; i++) {
+                let letter = secretWord[i];
+                if (letter === 'A' || letter === 'E' || letter === 'I' || letter === 'O' || letter === 'U') {
+                    vowels.push(letter);
+                }
+            }
+            
+            if (vowels.length > 0) {
+                let randomVowel = vowels[Math.floor(Math.random() * vowels.length)];
+                resultArea.innerText = "Correct! A vowel in the word is: " + randomVowel;
+            } else {
+                resultArea.innerText = "Correct! There are no vowels in this word.";
+            }
+        } else {
+            resultArea.innerText = "Wrong answer! Try again.";
+        }
+    });
+}
+
+function showLetterGame() {
+    // List of words to scramble
+    let wordsToScramble = ["GAMES", "MUSIC", "BOOKS", "PHONE", "WATER", "HAPPY", "SMILE", "DANCE", "MAGIC", "LIGHT"];
+    
+    // Pick a random word
+    let originalWord = wordsToScramble[Math.floor(Math.random() * wordsToScramble.length)];
+    
+    // Function to scramble the word
+    function scrambleWord(word) {
+        let letters = word.split(''); // Turn word into array of letters
+        
+        // Shuffle the letters randomly
+        for (let i = letters.length - 1; i > 0; i--) {
+            let randomIndex = Math.floor(Math.random() * (i + 1));
+            // Swap letters
+            let temp = letters[i];
+            letters[i] = letters[randomIndex];
+            letters[randomIndex] = temp;
+        }
+        
+        return letters.join(''); // Turn back into word
+    }
+    
+    let scrambledWord = scrambleWord(originalWord);
+    
+    // Make sure scrambled word is different from original
+    while (scrambledWord === originalWord) {
+        scrambledWord = scrambleWord(originalWord);
+    }
+    
+    minigameArea.innerHTML = `
+        <h3>Letter Hint Minigame</h3>
+        <p>Unscramble this word to get a letter hint:</p>
+        <p>Scrambled: <strong>${scrambledWord}</strong></p>
+        <input type="text" id="letterAnswer" placeholder="Unscrambled word">
+        <button id="letterSubmit">Submit</button>
+        <p id="letterResult"></p>
+    `;
+    
+    popup.style.display = "block";
+    
+    let letterSubmitBtn = document.getElementById("letterSubmit");
+    letterSubmitBtn.addEventListener("click", function() {
+        let playerAnswer = document.getElementById("letterAnswer").value.toUpperCase();
+        let resultArea = document.getElementById("letterResult");
+        
+        if (playerAnswer === originalWord) {
+            // Pick a random letter from secret word
+            let randomIndex = Math.floor(Math.random() * secretWord.length);
+            let randomLetter = secretWord[randomIndex];
+            resultArea.innerText = "Correct! A letter in the word is: " + randomLetter;
+        } else {
+            resultArea.innerText = "Wrong answer! The correct word was: " + originalWord + ". Try again!";
+        }
+    });
+}
+
+function showConsonantGame() {
+    // Picks a random number of dots between 5 and 15
+    let numDots = Math.floor(Math.random() * 11) + 5;
+    
+    // Creates the dots string
+    let dots = "";
+    for (let i = 0; i < numDots; i++) {
+        dots = dots + "• ";
+    }
+    
+    minigameArea.innerHTML = `
+        <h3>Consonant Hint Minigame</h3>
+        <p>Count the dots to get a consonant hint:</p>
+        <p style="font-size: 20px; line-height: 1.5;">${dots}</p>
+        <input type="number" id="consonantAnswer" placeholder="How many dots?">
+        <button id="consonantSubmit">Submit</button>
+        <p id="consonantResult"></p>
+    `;
+    
+    popup.style.display = "block";
+    
+    let consonantSubmitBtn = document.getElementById("consonantSubmit");
+    consonantSubmitBtn.addEventListener("click", function() {
+        let playerAnswer = document.getElementById("consonantAnswer").value;
+        let resultArea = document.getElementById("consonantResult");
+        
+        if (parseInt(playerAnswer) === numDots) {
+            // Find consonants in secret word
+            let consonants = [];
+            for (let i = 0; i < secretWord.length; i++) {
+                let letter = secretWord[i];
+                if (letter !== 'A' && letter !== 'E' && letter !== 'I' && letter !== 'O' && letter !== 'U') {
+                    consonants.push(letter);
+                }
+            }
+            
+            if (consonants.length > 0) {
+                // randomConsonant equals random consonant in the word
+                let randomConsonant = consonants[Math.floor(Math.random() * consonants.length)];
+                
+                // gives random consonant in word if you answer minigame correctly
+                resultArea.innerText = "Correct! A consonant in the word is: " + randomConsonant;
+
+                // if you get the minigame correct but there's no consonants in the word
+            } else {
+                resultArea.innerText = "Correct! There are no consonants in this word.";
+            }
+            // if you get the minigame incorrect it will show this message
+        } else {
+            resultArea.innerText = "Wrong answer! There were " + numDots + " dots. Try again!";
+        }
+    });
 }
